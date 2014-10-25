6824a8a34dd8dc222c7ef82232be53ab9ea5859d
Kristine Robison <krobison@gmail.com>
2014-10-25 13:05:01 -0700

    Merging with Raj and Daniel

--- app/controllers/asteroids_controller.rb
+++ app/controllers/asteroids_controller.rb
@@ -1,7 +1,5 @@
 class AsteroidsController < ApplicationController
-  respond_to :json
   def index
-    @asteroids = Asteroid.where(neo: true)
-    render json: @asteroids.map(&:asteroid_packet)
+
   end
 end
