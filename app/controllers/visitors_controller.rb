12a77ac0a12e894b6d4bdfbf7e90dc5ab9092244
Kristine Robison <kris.robison@oracle.com>
2015-05-20 16:00:43 -0700

    Try to overcome Heroku issue

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,10 +1,16 @@
 class VisitorsController < ApplicationController
 
+  before_filter :allow_iframe_requests
   def mpc
     render file: 'public/mpc/index.html'
   end
 
   def ae
     render file: 'public/ae/index.html'
+  end
+
+  private
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
   end
 end
