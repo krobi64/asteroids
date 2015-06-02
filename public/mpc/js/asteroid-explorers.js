6d7039b62707c77797ece155a72aa8e9bac8d1d5
danielone <daniel@6sense.com>
2015-06-02 00:26:46 -0700

    UI Update
    - updated index page
    - updated about page

--- public/mpc/js/asteroid-explorers.js
+++ public/mpc/js/asteroid-explorers.js
@@ -2,3 +2,27 @@
 function goBack() {
     window.history.back();
 }
+
+
+function questionFocus_in(ae_question) {
+
+    //make text have focus style 
+    var a = "ae-info-"+ae_question;
+    //make button have focus style
+    var b = "ae-btn-"+ae_question;
+    document.getElementById(a).className = "ae-text-explore-hover"; 
+    document.getElementById(b).className = "btn btn-block ae-btn-explore-hover";
+     
+}
+
+
+function questionFocus_out(ae_question) {
+
+    //make text have focus style 
+    var a = "ae-info-"+ae_question;
+    //make button have focus style
+    var b = "ae-btn-"+ae_question;
+    document.getElementById(a).className = "q-text"; 
+    document.getElementById(b).className = "btn btn-block ae-btn-explore";
+     
+}
