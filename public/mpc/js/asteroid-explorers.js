dcac64db87064962627878bc9fc45429f0a2a781
danielone <daniel@6sense.com>
2015-06-03 02:45:23 -0700

    Main UI Changes
    - new styles for index page
    - small tweaks to control panel twitter
    - removed captions from homepage slides

--- public/mpc/js/asteroid-explorers.js
+++ public/mpc/js/asteroid-explorers.js
@@ -7,11 +7,19 @@ function goBack() {
 function questionFocus_in(ae_question) {
 
     //make text have focus style 
-    var a = "ae-info-"+ae_question;
+    //var a = "ae-info-"+ae_question;
+    
     //make button have focus style
     var b = "ae-btn-"+ae_question;
-    document.getElementById(a).className = "ae-text-explore-hover"; 
-    document.getElementById(b).className = "btn btn-block ae-btn-explore-hover";
+    
+    //make quotes ahve focus style
+    var c1 = "ae-quote-"+ae_question;
+    var c2 = "ae-quote-"+ae_question+"-end";
+
+    //document.getElementById(a).className = "ae-text-explore-hover";
+    document.getElementById(b).className = "btn btn-block ae-btn-explore-hover"; 
+    document.getElementById(c1).className = "q-quote-hover";
+    document.getElementById(c2).className = "q-quote-hover";
      
 }
 
@@ -19,10 +27,18 @@ function questionFocus_in(ae_question) {
 function questionFocus_out(ae_question) {
 
     //make text have focus style 
-    var a = "ae-info-"+ae_question;
+    //var a = "ae-info-"+ae_question;
+    
     //make button have focus style
     var b = "ae-btn-"+ae_question;
-    document.getElementById(a).className = "q-text"; 
+
+     //make quotes ahve focus style
+    var c1 = "ae-quote-"+ae_question;
+    var c2 = "ae-quote-"+ae_question+"-end";
+    
+    //document.getElementById(a).className = "q-text"; 
     document.getElementById(b).className = "btn btn-block ae-btn-explore";
+    document.getElementById(c1).className = "q-quote";
+    document.getElementById(c2).className = "q-quote";
      
 }
